6cdcdc5b873359abdaf22ee5cf26bdcd1b70085a
AltoVoltaje1101 <felix.itiel@uabc.edu.mx>
2022-12-23 14:32:51 -0800

    First ver1.0

--- src/components/cards/TypeScript.js
+++ src/components/cards/TypeScript.js
@@ -6,7 +6,7 @@ export const TypeScript = ({ typeArray }) => { //Return pokemon type icons
     if (typeArray.length > 1) {
         return (
             <td><img src={'icons/'+typeArray[0].type.name+'.svg'} style={{height:'20px'}} alt="first-type" title={capitalize(typeArray[0].type.name)}></img>
-            <img src={'icons/'+typeArray[1].type.name+'.svg'} style={{height:'20px'}} alt="second-type" title={capitalize(typeArray[1].type.name)}></img></td>
+             <img src={'icons/'+typeArray[1].type.name+'.svg'} style={{height:'20px'}} alt="second-type" title={capitalize(typeArray[1].type.name)}></img></td>
         )
     } else return (
         <td><img src={'icons/'+typeArray[0].type.name+'.svg'} style={{height:'20px'}} alt="first-type" title={capitalize(typeArray[0].type.name)}></img></td>
